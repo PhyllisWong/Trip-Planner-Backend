619970ad3af680884b1d0a7e5543dd5eb424fc82
Phyllis Wong <wong.phyllism@gmail.com>
2017-11-18 12:11:26 -0800

    fix put method

--- test_trip_server.py
+++ test_trip_server.py
@@ -24,26 +24,24 @@ class FlaskrTestCase(unittest.TestCase):
         db.drop_collection('users')
 
     def test_getting_a_user(self):
-
-        ## 2 Post user to database
+        ## 2 Post user to database to test GET request
         self.app.post('/users',
                           headers=None,
                           data=json.dumps(dict(
                               name="Chris Mauldin",
                               email="chris@example.com"
-                          )),
+                          ) ),
                           content_type='application/json')
 
         ## 3 Make a get request to fetch the posted user
 
         response = self.app.get('/users',
-                                query_string=dict(email="chris@example.com")
-                            )
+                                query_string=dict(email="chris@example.com") )
 
         # Decode reponse
-        response_json = json.loads(response.data.decode())
+        response_json = json.loads(response.data.decode() )
 
-        ## Actual test to see if GET request was succesful
+        ## Test to see if GET request was succesful
         ## Here we check the status code
         self.assertEqual(response.status_code, 200)
 
@@ -55,26 +53,24 @@ class FlaskrTestCase(unittest.TestCase):
                           data=json.dumps(dict(
                               name="Phyllis Wong",
                               email="Phyllis@example.com"
-                          )),
+                          ) ),
                           content_type='application/json')
-
 
         self.app.put('/users',
                         headers=None,
                         data=json.dumps(dict(
                             name="Phyllis Smith",
                             email="Phyllis@gmail.com"
-                        )),
+                        ) ),
                         content_type='application/json')
 
-
         response = self.app.get('/users',
-                                query_string=dict(email="Phyllis@gmail.com")
-                            )
+                                query_string=dict(email="Phyllis@gmail.com") )
 
-        response_json = json.loads(response.data.decode())
+        response_json = json.loads(response.data.decode() )
 
-
+        ## Test to see if PUT request was succesful
+        ## Here we check the status code
         self.assertEqual(response.status_code, 200)
 
 
