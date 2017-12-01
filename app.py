1db30edea0a3de13e6b76de579a5000136448b1a
Phyllis Wong <wong.phyllism@gmail.com>
2017-12-01 11:14:59 -0800

    add app and Procfile

--- app.py
+++ app.py
@@ -1,94 +1,173 @@
-import json
-import pdb
-from flask import Flask, request
-import datetime
-
-from bson import Binary, Code
-from bson.json_util import dumps
-
-# 1 -
+from flask import Flask, jsonify, request, make_response, g
+from flask_restful import Resource, Api
+from flask_pymongo import PyMongo
 from pymongo import MongoClient
+from bson.json_util import dumps
+from util import JSONEncoder
+import json
 
+
+import bcrypt
+import pdb
+
+# 1 - pymongo is a MongoAPI
 app = Flask(__name__)
-# Flask auto updates any changes with this code
 app.config['DEBUG'] = True
 
 
-# 2 - Pass in hostname and port
-mongo = MongoClient('localhost', 27017)
+mongo = MongoClient('mongodb://phyllisWong:test@ds139909.mlab.com:39909/trip_planner_pro')
+app.db = mongo.trip_planner_pro
+app.bcrypt_rounds = 5
+
+class User(Resource):
+    def post(self): # This WORKS!!!!!!!
+        # Get data from the body of the http request
+        new_user = request.json
+        users_collection = app.db.users
+
+        # save the value of the data in a variable
+        email = new_user["email"]
+        password = new_user["password"]
+
+        # check if the user already exists in the database
+        check_for_user = users_collection.find_one({'email': email})
+
+        # if the email already exists in the database
+        # alert user that the email has been used
+        if check_for_user is not None:
+            return ('This email is already in use.', 401, None)
+
+        # if the email doesn't exist
+        # store email and encode and hash password to be saved in database
+        else:
+            encoded_password = password.encode('utf-8')
+
+            # base64 encoding handled by the bcrypt library
+            hashed = bcrypt.hashpw(
+                encoded_password, bcrypt.gensalt(app.bcrypt_rounds)
+                )
+            new_user['password'] = hashed.decode()
+            # insert user and user details to database
+            result = users_collection.insert_one(new_user)
+
+        # pdb.set_trace()
+        return ('New user has been added.', 200, None)
+
+    def get(self): # THIS WORKS!!!!
+        # pdb.set_trace()
+        users_collection = app.db.users
+        email = request.authorization.username
+        password = request.authorization.password
+
+        # encode the user input password to utf_8
+        encoded_password = password.encode('utf-8')
+
+        # find the user by the given email
+        check_saved_user = users_collection.find_one({'email': email})
+        # if user not in database, alert user to register not login
+        if check_saved_user is None:
+            return ('User does not exist', 404, None)
+
+        # find the password that matches the user entered password
+        get_saved_password = check_saved_user['password']
+        get_saved_password = get_saved_password.encode('utf-8')
+
+        if bcrypt.checkpw(encoded_password, get_saved_password) == True:
+            return ('Success', 200, None)
+        else:
+            return ('Bad request', 400, None)
+
+        # pdb.set_trace()
+
+    def put(self):
+        edit_user = request.json
+        email = request.args.get('email')
+        users_collection = app.db.users
+        if request.args.get('email'):
+            user = users_collection.find_one_and_replace(
+                {'email': email}, email)
+            return (user, 200, None)
+        else:
+            return ({"BAD REQUEST"}, 404, None)
 
 
-# 3 - specify the database
-app.db = mongo.test
+    # Future feature work on this later
+    def patch(self):
+        a_user = request.json
+        name = request.args.get('name')
+        users_collection = app.db.users
+        result = users_collection.find_one_and_update(
+            {'name': name},
+            {'$set': {'departDate': a_user_fav_foods} }
+        )
+
+        return (result, 200, None)
+
+    # Deletes a user
+    def delete(self):
+        a_user = request.json
+        name = request.args.get('name')
+        users_collection = app.db.users
+        result = users_collection.find_one_and_delete(
+            {'name': name}
+
+        )
+        return (a_user, 200, None)
 
 
-@app.route('/')
-def my_hello():
-    json_hello = 'Hello Make School!'
-    return (json_hello, 200, None)
+class Trip(Resource):
+    def post(self):
+        '''Create a new trip, store in the database.'''
+        a_trip = request.json
+        trips_collection = app.db.trips
+        result = trips_collection.insert_one(a_trip)
+        # pdb.set_trace()
+        return (a_trip, 200, None)
+
+    def get(self):
+        '''Return a trip from the database.'''
+        pdb.set_trace()
+        trips_collection = app.db.trips
+        destination = request.args.get('destination')
+
+        if request.args.get('destination'):
+            trip = trips_collection.find_one({'destination': destination})
+
+            return (trip, 200, None)
+        else:
+            return ({"BAD REQUEST"}, 404, None)
+        # pdb.set_trace()
+
+    def put(self):
+        '''Replace a trip with a new trip.'''
+        a_trip = request.json
+        destination = request.args.get('destination')
+        trips_collection = app.db.trips
+        if request.args.get('destination'):
+            trip = users_collection.find_one_and_replace(
+                {'destination': destination}, destination)
+            return (trip, 200, None)
+        else:
+            return ({"BAD REQUEST"}, 404, None)
+        pass
+
+    def patch(self):
+        '''Replace a detail of a trip.'''
+        pass
+
+    ''' add get request to find trip '''
+    ''' add data model for trips  '''
 
 
-@app.route('/users')
-def get_users():
+api = Api(app)
+api.add_resource(User, '/users')
+api.add_resource(Trip, '/trips')
 
-    #1 get URL params
-    name = request.args.get('name')
-
-    #2 Our users collection
-    users_collection = app.db.users
-
-    #3 Find one document in our users collections
-    result = users_collection.find_one(
-        {'name': name}
-    )
-
-    #4 Covert result to json, its initially a python dict
-    json_result = dumps(result)
-
-    #4.b Add a break point
-    # pdb.set_trace()
-    #5 Return the json as part of the response body
-    # param1 data, param2 result, param3 headers
-    return (json_result, 200, None)
-
-
-@app.route('/my_page')
-def my_page_route():
-    me = {'name': 'Phyllis', 'age': 100, 'hobby': 'snowboarding'}
-    json_me = json.dumps(me)
-    # pdb.set_trace()
-    return (json_me, 200, None)
-
-
-@app.route('/pets')
-def fav_pets():
-    pdb.set_trace()
-    pets = [
-        {
-            'name': 'Boba',
-            'breed': 'Dachshund',
-            'color': 'red'
-        },
-        {
-            'name': 'Princess',
-            'breed': 'Pitbull',
-            'color': 'black'
-        },
-        {
-            'name': 'Qui Qui',
-            'breed': 'House Cat',
-            'color': 'Grey Stripped'
-        },
-        {
-            'name': 'Kitty',
-            'breed': 'Chihuahua',
-            'color': 'Brown'
-        }
-    ]
-
-    json_pets = json.dumps(pets)
-    return (json_pets, 200, None)
-
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    resp = make_response(JSONEncoder().encode(data), code)
+    resp.headers.extend(headers or {})
+    return resp
 
 if __name__ == '__main__':
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
